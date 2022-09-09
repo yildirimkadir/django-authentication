f0c6b2f44903166675bf92fb3c823baa474d51e1
yildirimkadir <yildirimkad34@gmail.com>
2022-09-09 15:43:51 +0200

    "updated"

--- user_example/views.py
+++ user_example/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def home(request):
+    return render(request, "user_example/index.html")
+
+@login_required
+def special(request):
+    return render(request, "user_example/special.html")
+
+def register(request):
+    form= UserCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect("login")
+    
+    context = {
+        'form': form
+    }
+    return render(request, "user_example/register.html", context)
